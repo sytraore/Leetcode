34e671598a7cf8c75de9c44f3fb08ceaf1f7da98
Sy Traore <stephanetraore46@gmail.com>
2025-04-03 00:05:11 -0400

    Update ContainsDuplicates2

--- Java/Arrays/SlidingWindow/ContainDuplicates2.java
+++ Java/Arrays/SlidingWindow/ContainDuplicates2.java
@@ -1,29 +1,25 @@
 package Java.Arrays.SlidingWindow;
-
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class ContainDuplicates2{
 
     public static boolean containDuplicates(int [] nums, int k){
-        /*
-            check if the array contains duplicates within k distance
-            use a hashmap to store the index of each element
-            iterate through the array and check if the element is already in the hashmap
-            if it is, check if the difference between the current index and the index in the hashmap is <= k
-            if it is, return true
-            if it is not, update the index in the hashmap
-            if it is not in the hashmap, add it to the hashmap
-        */
+        // Use a HashSet to maintain the sliding window of size k
+        HashSet<Integer> window = new HashSet<>();
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++){
-            if (map.containsKey(nums[i])){
-                if (i - map.get(nums[i]) <= k){
-                    return true;
-                }
+        for (int i = 0; i < nums.length; i++) {
+            // If the current element is already in the window, we found a duplicate
+            if (window.contains(nums[i])) {
+                return true;
             }
-            map.put(nums[i], i);
+
+            // Add the current element to the window
+            window.add(nums[i]);
+
+            // If the window size exceeds k, remove the oldest element
+            if (window.size() > k) {
+                window.remove(nums[i - k]);
+            }
         }
 
         return false;
