0340c5aa8df1fc1bf7b70b2cf8d692e1d1924301
Sy Traore <stephanetraore46@gmail.com>
2025-04-05 11:03:26 -0400

    Merge sorted array java

---
+++ Java/Arrays/ClassicTwoPointers/MergeSortedArray.java
@@ -0,0 +1,26 @@
+package Java.Arrays.ClassicTwoPointers;
+public class MergeSortedArray {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        // implement brut force
+
+
+
+
+
+        
+        // int i = m - 1; // Pointer for nums1
+        // int j = n - 1; // Pointer for nums2
+        // int k = m + n - 1; // Pointer for the merged array (nums1)
+
+        // while (j >= 0) {
+        //     if (i >= 0 && nums1[i] > nums2[j]) {
+        //         nums1[k] = nums1[i];
+        //         i--;
+        //     } else {
+        //         nums1[k] = nums2[j];
+        //         j--;
+        //     }
+        //     k--;
+        // }
+    }
+}
