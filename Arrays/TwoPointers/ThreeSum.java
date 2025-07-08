f7097c3338989692c300d1195abafc9381f33dec
Sy Traore <stephanetraore46@gmail.com>
2025-07-08 00:43:45 -0400

    Refactor ThreeSum.java to reorganize solution comments
    Moved the optimal solution comment above the brute force implementation and restructured the code comments for clarity. No functional changes were made to the algorithm.

--- Arrays/TwoPointers/ThreeSum.java
+++ Arrays/TwoPointers/ThreeSum.java
@@ -7,17 +7,6 @@ import java.util.List;
 
 public class ThreeSum {
     public static List<List<Integer>> threeSum(int[] nums) {
-
-        /*
-            Optimal solution:
-                The sum of two numbers must cancel the third number for the sum of the three numbers be equal to 0.
-                Sort the array
-                for each element in the array, search for the two elements that will result in 0 if they get added to the current element.
-
-                Appropriate data structure: array
-                Appropriate algorithm: Two pointers
-         */
-        
         /*
             Brute force:
                 Sort the array to easily find the elements
@@ -28,27 +17,42 @@ public class ThreeSum {
                 Appropriate algorithm: Nested loops
                 Time complexity: O(n^3)
                 Space complexity: O(n)
-         */
-         
-        HashSet<List<Integer>> set = new HashSet<>();
-        int numsLen = nums.length;
-        Arrays.sort(nums);
 
-        for (int i = 0; i < numsLen; i++){
-            for (int j = i + 1; j < numsLen; j++){
-                for (int k = j + 1; k < numsLen; k++){
-                    if (nums[i] + nums[j] + nums[k] == 0){
-                        List<Integer> lst = new ArrayList<>();
-                        lst.add(nums[i]);
-                        lst.add(nums[j]);
-                        lst.add(nums[k]);
-                        set.add(lst);
+            
+            Implementation:
+
+            HashSet<List<Integer>> set = new HashSet<>();
+            int numsLen = nums.length;
+            Arrays.sort(nums);
+
+            for (int i = 0; i < numsLen; i++){
+                for (int j = i + 1; j < numsLen; j++){
+                    for (int k = j + 1; k < numsLen; k++){
+                        if (nums[i] + nums[j] + nums[k] == 0){
+                            List<Integer> lst = new ArrayList<>();
+                            lst.add(nums[i]);
+                            lst.add(nums[j]);
+                            lst.add(nums[k]);
+                            set.add(lst);
+                        }
                     }
                 }
             }
-        }
+            
+            return new ArrayList<>(set);
+         */
+
+        /*
+            Optimal solution:
+                The sum of two numbers must cancel the third number for the sum of the three numbers be equal to 0.
+                Sort the array
+                for each element in the array, search for the two elements that will result in 0 if they get added to the current element.
+
+                Appropriate data structure: array
+                Appropriate algorithm: Two pointers
+        */
+         
         
-        return new ArrayList<>(set);
     }
 
     public static void main(String[] args){
