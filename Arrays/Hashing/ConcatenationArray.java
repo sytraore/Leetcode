bf85953032e947539b216dfd6302e8502507da26
Sy Traore <stephanetraore46@gmail.com>
2025-07-23 00:52:44 -0400

    Add ConcatenationArray and clean up ThreeSum imports
    Added ConcatenationArray.java with a method to concatenate an array with itself. Removed unused HashSet import from ThreeSum.java for code cleanliness.

---
+++ Arrays/Hashing/ConcatenationArray.java
@@ -0,0 +1,39 @@
+package Arrays.Hashing;
+
+public class ConcatenationArray {
+    public static int[] getConcatenation(int[] nums) {
+        /**
+            use a pointer to track the end of nums
+            if the pointer is equal to nums.length -1, reset it to 0
+
+            Data structures: array
+            Time Complexity: O(n)
+            Space complexity: O(n)
+         */
+
+        int len = nums.length * 2;
+
+        if (len == 0){
+            return new int[0];
+        }
+
+        int[] ans = new int[len];
+        int ptr = 0;
+
+        for (int i = 0; i < len; i++){
+            if (i == nums.length){
+                ptr = 0;
+            }
+            ans[i] = nums[ptr];
+            ptr++;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        int[] result = getConcatenation(nums);
+        System.out.println(java.util.Arrays.toString(result)); // Output: [1, 2, 3, 1, 2, 3]
+    }
+}
--- Arrays/TwoPointers/ThreeSum.java
+++ Arrays/TwoPointers/ThreeSum.java
@@ -2,7 +2,6 @@ package Arrays.TwoPointers;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 
 public class ThreeSum {
